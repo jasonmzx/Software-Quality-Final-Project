0aeb1ddf856e518517b9a9eb58d339573d9959ba
jasonmzx <jasonmvocet@gmail.com>
2024-03-14 09:59:06 -0400

    Booking View Template Facelift + Singleton In Mem. DB + Basic Sync of backend data onto frontend view

--- SOFE3980U_Final_Project/src/main/java/com.ontariotechu.sofe3980U/BookingViewController.java
+++ SOFE3980U_Final_Project/src/main/java/com.ontariotechu.sofe3980U/BookingViewController.java
@@ -6,15 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// Model Imports:
+import com.ontariotechu.sofe3980U.core.MemoryStore;
+
 @Controller
 public class BookingViewController {
 
-	@GetMapping("/")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		
-		model.addAttribute("name", name);
-		
-		return "booking_view";
-	}
+    @GetMapping("/")
+    public String home(Model model) {
+
+        MemoryStore memoryStore = MemoryStore.getInstance(); //get singleton inst
+        
+        model.addAttribute("airports", memoryStore.getAirportList()); // add "airports" attrib to view template
+        
+        return "booking_view";
+    }
 
 }
---
+++ SOFE3980U_Final_Project/src/main/java/com.ontariotechu.sofe3980U/core/MemoryStore.java
@@ -0,0 +1,34 @@
+package com.ontariotechu.sofe3980U.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryStore {
+
+    private static MemoryStore instance = null; // SINGLETON INSTANCE
+    private List<Airport> airportsList;
+
+    private MemoryStore() { //Private Constructor, as we'll only be building this object here
+        airportsList = new ArrayList<>();
+
+        // Populate the list with some airports
+        airportsList.add(new Airport("John F. Kennedy International Airport", "America/New_York", 1));
+        airportsList.add(new Airport("Los Angeles International Airport", "America/Los_Angeles", 2));
+        airportsList.add(new Airport("Chicago O'Hare International Airport", "America/Chicago", 3));
+        airportsList.add(new Airport("Heathrow Airport", "Europe/London", 4));
+    }
+
+    public static synchronized MemoryStore getInstance() {
+        if (instance == null) {
+            instance = new MemoryStore();
+        }
+        return instance;
+    }
+
+    // -------------- Getters & Setters ---------------------------
+
+        public List<Airport> getAirportList() {
+        return airportsList;
+    }
+
+}
